4e4926fbdce4ce730291efe7ea9b64b4ec00d5ac
h-ivandjikov <h.ivandjikov03@gmail.com>
2026-05-03 23:19:53 +0300

    change nvim pop-up window size

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+    },
+    views = {
+      hover = {
+        size = {
+          max_height = 10,
+          max_width = 80,
+        },
+      },
+    },
+  },
+}
