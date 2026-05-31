67b6de4885d2f46dc6f23fd3c46c9f4e45a9a6dc
h-ivandjikov <h.ivandjikov03@gmail.com>
2026-05-31 23:48:24 +0300

    change side scrolloff length

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 999
-vim.opt.sidescrolloff = 999
+vim.opt.sidescrolloff = 30
 vim.opt.shiftwidth = 4
